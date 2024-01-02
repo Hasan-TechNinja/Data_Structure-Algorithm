90034bfc9137f59b5cd9d92301cd11df9fefa1d4
Hasan-Error-Code <hasan.error.code@gmail.com>
2024-01-02 08:58:00 +0600

    first

---
+++ sum.py
@@ -0,0 +1,4 @@
+number_first = 20
+number_second = 30
+sum = number_first + number_second
+print(sum)
---
+++ sum2.py
@@ -0,0 +1,5 @@
+
+num1 = int(input("Enter a number: "))
+sum = num1 * (num1 + 1) * 2
+print(sum)
+
