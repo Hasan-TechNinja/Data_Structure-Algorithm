9a8f79fbd03f65fa4c0f5010784ee54901831bfd
Hasan-Error-Code <hasan.error.code@gmail.com>
2024-01-02 22:02:29 +0600

    m

--- Search_Algorithm.py
+++ Search_Algorithm.py
@@ -1,4 +1,6 @@
 def Linear_search(L, x):
+    L = [1,5,7,3,5,2,3,9,8,7,4,5,4,3]
+    x = int(input('Enter a number: '))
     n = len(L)
     i = 0
 
@@ -9,6 +11,8 @@ def Linear_search(L, x):
     i = -1
     return i
 
+Linear_search()
+
 def Linear_Search(L, x):
     n = len(L)
 
@@ -17,6 +21,9 @@ def Linear_Search(L, x):
             return i
     return -1
 
-L = [1, 2, 3, 9, 8, 4, 3, 9]
-x = int(input("Enter a integer: "))
-Linear_search()
+def Binary_search(L, x):
+    n = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+    m = 4
+
+    left, right = 0, len(L) - 1
+    
