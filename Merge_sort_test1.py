a93749c3a84ca2a503a8212b03d0e9bd9b6c3c45
Hasan-Error-Code <hasan.error.code@gmail.com>
2024-01-04 11:38:19 +0600

    Merge sort test 1 ---

--- Merge_sort_test1.py
+++ Merge_sort_test1.py
@@ -1,11 +1,19 @@
-def Merge_sort(list1):
-    if len(list1) > 1:
+def merge_sort(list1):
+    if len(list1) > 1: 
         mid = len(list1) // 2
-        left = list[:mid]
-        right = list[mid:]
-        Merge_sort(left)
-        Merge_sort(right)
+        left_list = list1[:mid]
+        right_list = list1[mid:]
+        merge_sort(left_list)
+        merge_sort(right_list)
 
+        i, j, k = 0, 0, 0
 
-ls = [int(input("Enter a list: "))]
-Merge_sort(ls)
+        while i < len(left_list) and j < len(right_list):
+            if left_list[i] < right_list[j]:
+                list1[k] = left_list[1]
+                i += 1
+                k += 1
+            else:
+                list1[k] = right_list[j]
+                j += 1
+                k += 1
