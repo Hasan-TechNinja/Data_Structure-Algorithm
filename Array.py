834946978386729e328a93e80e98bd57bf821659
Hasan-Error-Code <hasan.error.code@gmail.com>
2024-01-04 19:13:46 +0600

    array

---
+++ Array.py
@@ -0,0 +1,3 @@
+expense = [2200, 2350, 2600, 2130, 2190]
+print(f"1. In feb {expense[1]-expense[0]} dollars I spent extra compare to january")
+print(f"The first three month total expense {expense[0] + expense[1] + expense[2]}")
